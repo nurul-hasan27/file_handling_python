d1be99494c31d749eabb813376fd3efc42b45925
Nurul Hasan <mdnurulhasan1111@gmail.com>
2026-09-05 06:36:14 +0530

    Add update file feature

--- main.py
+++ main.py
@@ -85,8 +85,55 @@ def readfile():
 
 
 def updatefile():
-    readfielandfolder()
-    print("run updatefile")
+    try:
+
+        readfielandfolder()
+
+        name = input(
+            "Enter the file name or file path from root directory :- "
+        )
+
+        root = Path.cwd()
+        p = root / name
+
+        if p.exists() and p.is_file():
+            print("press 1 for changing the name of your file :- ")
+            print("press 2 for overwriting the data of your file")
+            print("press 3 for appending some content in your file")
+
+            res = int(input("tell your response :- "))
+
+            if res == 1:
+                name2 = input(
+                    "Enter your new file name or file path from root directory :- "
+                )
+
+                p2 = root / name2
+                p.rename(p2)
+
+                print("File renamed successfully")
+
+            if res == 2:
+                with open(p, "w") as fs:
+                    data = input(
+                        "tell what you want to write this is overwrite the data :- "
+                    )
+                    fs.write(data)
+
+                print("File data overwritten successfully")
+
+            if res == 3:
+                with open(p, "a") as fs:
+                    data = input("tell what you want to append :- ")
+                    fs.write(" " + data)
+
+                print("Content appended successfully")
+
+        else:
+            print("The file does not exist")
+
+    except Exception as err:
+        print(f"An error occurred as {err}")
 
 
 def deletefile():
